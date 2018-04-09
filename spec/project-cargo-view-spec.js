79a94f4ec36c9f59ad4b5c9212f5aea88a3ad994
Evan Behar <behare@gmail.com>
2018-04-08 19:18:10 -0700

    First commit

---
+++ spec/project-cargo-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CargoProjectView from '../lib/project-cargo-view';
+
+describe('CargoProjectView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
